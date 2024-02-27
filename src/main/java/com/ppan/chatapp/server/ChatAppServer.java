22b5893d29469ec9eeaccc6075bdd85c56c537e0
piyush.pandey <piyush.pandey@clevertap.com>
2024-02-28 03:01:48 +0530

    Sending msgType so client can render them differently

--- src/main/java/com/ppan/chatapp/server/ChatAppServer.java
+++ src/main/java/com/ppan/chatapp/server/ChatAppServer.java
@@ -3,6 +3,7 @@ package com.ppan.chatapp.server;
 import com.mongodb.BasicDBObject;
 import com.ppan.chatapp.model.User;
 import com.ppan.chatapp.utils.Constants;
+import com.ppan.chatapp.utils.Constants.MessageType;
 import jakarta.websocket.OnError;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
@@ -18,19 +19,18 @@ import java.util.logging.Logger;
 public class ChatAppServer {
     @OnOpen
     public void open(Session session) {
-        Logger.getLogger(ChatAppServer.class.getName()).log(Level.INFO, session.getRequestURI().toString());
-        Logger.getLogger(ChatAppServer.class.getName()).log(Level.INFO, session.getId() + " joined!");
         String username = session.getRequestParameterMap().get("username").get(0);
         if (username == null) {
-            sendMsg(session, "username is required");
+            sendErrorMsg(session, "Username is required");
             closeSession(session);
         } else {
             if (!ChatAppSessionHandler.getInstance().usernameExists(username)) {
                 String sessionId = session.getId();
                 ChatAppSessionHandler.getInstance().addSession(session);
                 ChatAppSessionHandler.getInstance().addUser(sessionId, new User(sessionId, username));
+                broadcastMsgToAll(Constants.SYSTEM_USERNAME, username + " joined");
             } else {
-                sendMsg(session, "username already exists");
+                sendErrorMsg(session, "Username already exists!");
                 closeSession(session);
             }
         }
@@ -38,9 +38,11 @@ public class ChatAppServer {
 
     @OnClose
     public void close(Session session) {
-        Logger.getLogger(ChatAppServer.class.getName()).log(Level.INFO, session.getId() + " left!");
+        String username = ChatAppSessionHandler.getInstance().getUsernameForId(session.getId());
+        sendInfoMsg(session, "Disconnected");
         ChatAppSessionHandler.getInstance().removeSession(session);
         ChatAppSessionHandler.getInstance().removeUser(session.getId());
+        broadcastMsgToAll(Constants.SYSTEM_USERNAME, username + " left");
     }
 
     @OnError
@@ -51,14 +53,27 @@ public class ChatAppServer {
 
     @OnMessage
     public void handleMessage(String message, Session session) {
-        String serializedMsg = new BasicDBObject(Constants.SENDER_KEY, ChatAppSessionHandler.getInstance().getUsernameForId(session.getId()))
-                .append(Constants.MSG_KEY, message).toString();
-        Logger.getLogger(ChatAppServer.class.getName()).log(Level.INFO, String.format("%s says \"%s\"", session.getId(), serializedMsg));
-        broadcastMsgToAll(serializedMsg);
+        String username = ChatAppSessionHandler.getInstance().getUsernameForId(session.getId());
+        broadcastMsgToAll(username, message);
     }
 
-    private void broadcastMsgToAll(String message) {
-        ChatAppSessionHandler.getInstance().sendMsgToAll(message);
+    private void broadcastMsgToAll(String sender, String message) {
+        String serializedMsg = new BasicDBObject(Constants.SENDER_KEY, sender)
+                .append(Constants.MSG_KEY, message)
+                .append(Constants.MSG_TYPE_KEY, MessageType.MESSAGE).toString();
+        ChatAppSessionHandler.getInstance().sendMsgToAll(serializedMsg);
+    }
+
+    private void sendErrorMsg(Session session, String msg) {
+        String serializedMsg = new BasicDBObject(Constants.MSG_KEY, msg)
+                .append(Constants.MSG_TYPE_KEY, MessageType.ERROR).toString();
+        sendMsg(session, serializedMsg);
+    }
+
+    private void sendInfoMsg(Session session, String msg) {
+        String serializedMsg = new BasicDBObject(Constants.MSG_KEY, msg)
+                .append(Constants.MSG_TYPE_KEY, MessageType.INFO).toString();
+        sendMsg(session, serializedMsg);
     }
 
     public static void sendMsg(Session session, String msg) {
@@ -70,7 +85,7 @@ public class ChatAppServer {
         }
     }
 
-    public void closeSession(Session session) {
+    private void closeSession(Session session) {
         try {
             session.close();
         } catch (IOException e) {
--- src/main/java/com/ppan/chatapp/utils/Constants.java
+++ src/main/java/com/ppan/chatapp/utils/Constants.java
@@ -7,4 +7,12 @@ import lombok.NoArgsConstructor;
 public class Constants {
     public static final String SENDER_KEY = "sender";
     public static final String MSG_KEY = "msg";
+    public static final String MSG_TYPE_KEY = "mType";
+    public static final String SYSTEM_USERNAME = "system";
+
+    public enum MessageType {
+        MESSAGE,
+        INFO,
+        ERROR
+    }
 }
